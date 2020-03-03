a942895c2a383f5b16c73941672993ec54deca17
Abraham <45113445+abrahamibli@users.noreply.github.com>
2020-03-03 11:19:18 -0700

    Nombres tarea 6

--- JS/Tarea 6/index.js
+++ JS/Tarea 6/index.js
@@ -1,3 +1,8 @@
+//Encender y apagar led con js
+//Uriel Omar Gonzalez Jimenez 320736
+//Daniel Alberto Zapata Jimenez 299474
+//Abraham Ibarra Linares 320861
+
 var five = require('johnny-five');
 var board = new five.Board();
 
